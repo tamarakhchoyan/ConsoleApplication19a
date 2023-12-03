4839563ad1fc9aecc11ccffd977dcdf3b8b2cf20
User <User@DESKTOP-9TGTEUB>
2023-12-04 00:36:11 +0400

    Add project files.

---
+++ ConsoleApplication19a/ConsoleApplication19a.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+int main()
+{
+	int x, y;
+	std::cin >> x >> y;
+	if (x > 0 && y > 0)
+	{
+		std::cout << 1;
+	}
+	if (x < 0 && y>0)
+	{
+		std::cout << 2;
+	}
+	if (x > 0 && y < 0)
+	{
+		std::cout << 4;
+	}
+	if (x < 0 && y < 0)
+	{
+		std::cout << 3;
+	}
+	if(x==0 && y==0)
+	{
+		std::cout << 0;
+	}
+	if(x==0 && y!=0)
+	{
+		std::cout << "OY";
+	}
+	if(x!=0 && y==0)
+	{
+		std::cout << "OX";
+	}
+	return 0;
+}
